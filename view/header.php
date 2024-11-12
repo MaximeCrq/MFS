42507cbb005ecbaa2751c28731410d75f9eddd75
floryanne <floryanne.puech31@gmail.com>
2024-11-12 16:33:20 +0100

    ajout header

---
+++ view/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <LINK rel="stylesheet" type="text/css" href="./css/compt.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <nav>
+        <img src="./image/2.png">
+        <ul>
+            <li><a href="">ACCUEIL</a></li>
+            <li><a href="">LISTE DES QUIZ</a></li>
+            <li><a href="">STATISTIQUE</a></li>
+            <li><a href="">INSCRIPTION</a></li>
+            <li><a href="">CONNEXION</a></li>
+            <img src="./image/Promojardin-icon-login.png">
+        </ul>
+    </nav>
+    
+    </header>
+
+
+
+
+
+</body>
+</body>
+</html>
