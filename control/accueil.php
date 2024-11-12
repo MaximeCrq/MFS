f882d38d0cf7c899cab563aec987c4e626ce7fbf
Maxime <maximecq@gmail.com>
2024-11-12 16:13:29 +0100

    modif model control

--- control/accueil.php
+++ control/accueil.php
@@ -1,5 +1,7 @@
 <?php
 
-class ControlerAccueil
+class ControllerAccueil {
+
+}
 
 ?>
--- model/model_users.php
+++ model/model_users.php
@@ -1,17 +1,87 @@
 <?php
 class ModelUser{
-    //Attribut
-    private ?int $idUser;
-    private ?string $nameUser;
-    private ?string $firstNameUser;
-    private ?string $loginUser;
-    private ?string $mdpUser;
+    private ?int $id;
+    private ?string $firstname;
+    private ?string $lastname;
+    private ?string $email;
+    private ?string $password;
+    private ?array $roles;
 
-    
-
-    //Constructeur
-    public function __construct(?string $login){
-        $this->loginUser = $login;
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(?array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    //Constructeur
+    public function __construct(){
+        $id="";
+        $firstname="";
+        $lastname="";
+        $email="";
+        $password="";
+        $roles="";
+    }
+
+    
 }
