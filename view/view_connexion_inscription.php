0bf2ccc34e36d5805341fd18c5307ffd388430f2
floryanne <floryanne.puech31@gmail.com>
2024-11-13 16:54:39 +0100

    modification

--- view/view_connexion_inscription.php
+++ view/view_connexion_inscription.php
@@ -1,13 +1,17 @@
 <main>
     <form action="" method="Post">
-        <label for="nom">nom</label>
-        <input type="text" name="nom"/>
-        <label for="prenom">prénom</label>
-        <input type="text" name="prenom"/>
-        <label for="mail">mail</label>
-        <input type="email" name="mail"/>
-        <label for="mdp">mot de pass</label>
-        <input type="password" name="mdp"/>
+        <label for="nom">nom
+            <input type="text" name="nom"/>
+        </label>
+        <label for="prenom">prénom
+            <input type="text" name="prenom"/>
+        </label>
+        <label for="mail">mail
+            <input type="email" name="mail"/>
+        </label>
+        <label for="mdp">mot de pass
+            <input type="password" name="mdp"/>
+        </label>
         <label for="vérifMdp">mail</label>
         <input type="password" name="vérifMdp"/>
         <input type="submit" name ="submit"/>
