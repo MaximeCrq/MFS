c73ce5728d01b3123d3506809c3e922882521c53
floryanne <floryanne.puech31@gmail.com>
2024-11-13 16:49:08 +0100

    ajout d'input

--- view/view_connexion_inscription.php
+++ view/view_connexion_inscription.php
@@ -6,6 +6,10 @@
         <input type="text" name="prenom"/>
         <label for="mail">mail</label>
         <input type="email" name="mail"/>
+        <label for="mdp">mot de pass</label>
+        <input type="password" name="mdp"/>
+        <label for="vérifMdp">mail</label>
+        <input type="password" name="vérifMdp"/>
         <input type="submit" name ="submit"/>
     </form>
 </main>
