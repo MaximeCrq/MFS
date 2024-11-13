af0d8ac22e778f3436faa59bd38deae1f46a1d07
floryanne <floryanne.puech31@gmail.com>
2024-11-13 14:53:23 +0100

    création compte

---
+++ view/view_compte.php
@@ -0,0 +1,22 @@
+<main>
+    <h1>Compte</h1>
+    <div id="compte">
+        <section>
+            <img href="">
+            <h3>avatar</h3>
+            <input type="submit" name="avatar">
+        </section>
+        <section>
+            <h3>Pseudo</h3>
+            <input type="submit" name="avatar">
+        </section>
+        <section>
+            <h3>Adresse mail actuelle</h3>
+            <input type="submit" name="avatar">
+        </section>
+        <section>
+            <h3>mot de passe actuel</h3>
+            <input type="submit" name="avatar">
+        </section>
+    </div>
+</main>
