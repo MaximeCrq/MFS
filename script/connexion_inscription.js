28b003f2d820d4cb270f7fbb8d62f6410a558ea4
floryanne <floryanne.puech31@gmail.com>
2024-11-14 12:18:56 +0100

    ajout js

--- script/connexion_inscription.js
+++ script/connexion_inscription.js
@@ -1,13 +1,13 @@
-let formulaire_un=document.querySelector(".formulaireUN");
-console.log(formulaire_un);
+const formulaire_un = document.querySelector(".formulaireUN");
+//console.log(formulaire_un);
 
-let formulaire_deux=document.querySelector(".formulaireDEUX");
-console.log(formulaire_deux);
+const formulaire_deux = document.querySelector(".formulaireDEUX");
+//console.log(formulaire_deux);
 
-let para=document.querySelector(".p_form_ins");
-console.log(para);
+const para = document.querySelector(".p_form_ins");
+//console.log(para);
 
 para.addEventListener('click',() =>{
-    formulaire_un.classList.toggle("afficher")
+    formulaire_un[0].classList.toggle("afficher")
    
 })
