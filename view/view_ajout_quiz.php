8d4bb2b293ab118586d4965532f3d5b591ea3bb8
floryanne <floryanne.puech31@gmail.com>
2024-11-14 16:02:13 +0100

    dernier

--- view/view_ajout_quiz.php
+++ view/view_ajout_quiz.php
@@ -3,30 +3,32 @@
     <div id="contenu">
       <h1>Création d’un quiz</h1>
       <form>
-        <select name="theme" id="theme">
-          <option value="PHP">PHP</option>
-          <option value="JS">JS</option>
-          <option value="MCD">MCD</option>
-          <option value="MLD">MLD</option>
-          <option value="HTMLetCSS">HTML et CSS</option>
-        </select>
-        <section>
-          <label for="mdp">Question</label>
-          <input type="text" name="questionPose"/>
-        </section>
-        <section>
-          <label for="mdp">Bonne raiponse</label>
-          <input type="text" name="reponseBonne"/>
-        </section>
-        <section>
-          <label for="mdp">mauvaise réponse</label>
-          <input type="text" name="reponseMauvaise"/>
-        </section>
-        <section>
-          <label for="mdp">mauvaise réponse</label>
-          <input type="text" name="reponseMauvaise"/>
-        </section>
-        <input type="submit" name="submit" id="submit"/>
+        <div id="créa_quiz">
+          <select name="theme" id="theme">
+            <option value="PHP">PHP</option>
+            <option value="JS">JS</option>
+            <option value="MCD">MCD</option>
+            <option value="MLD">MLD</option>
+            <option value="HTMLetCSS">HTML et CSS</option>
+          </select>
+          <section>
+            <label for="mdp">Question</label>
+            <input type="text" name="questionPose"/>
+          </section>
+          <section>
+            <label for="mdp">Bonne réponse</label>
+            <input type="text" name="reponseBonne"/>
+          </section>
+          <section>
+            <label for="mdp">Mauvaise réponse</label>
+            <input type="text" name="reponseMauvaise"/>
+          </section>
+          <section>
+            <label for="mdp">Mauvaise réponse</label>
+            <input type="text" name="reponseMauvaise"/>
+          </section>
+          <input type="submit" name="submit" id="submit" value="CRÉER"/>
+        </div>
       </form>
     </div>
   </main>
