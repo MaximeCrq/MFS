2bf9b9906e9dddc1d748823ed9401d595fe1b17c
Maxime <maximecq@gmail.com>
2024-11-18 11:50:52 +0100

    modification index

--- index.php
+++ index.php
@@ -134,7 +134,9 @@ switch($path){
         break;
     case '/MFS/liste_utilisateur' :
         //inclure mes views
-        $header-> displayNav();
+        include './control/liste_user.php';
+        $user2=new ControlerList;
+        $user2->listUser();
         $style='liste_utilisateur';
         $script='liste_utilisateur';
         include './view/view_header.php';
